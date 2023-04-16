9a98e5db1d70241387418e7e231df88ac5e1b59c
Jass <56713532+dev-kakashi@users.noreply.github.com>
2023-04-16 16:14:52 +0530

    Update constant.js

--- nextjs-blog/constant.js
+++ nextjs-blog/constant.js
@@ -1,36 +1,29 @@
 export const cards = [
   {
-    text: (
-      <p>
+    text: `
         A comprehensive exam includes a vision check as well as complete health
         examination of the front and back of your eyes.
-        <br />
+
         We provide the best care for your eyes which includes; full
         comprehensive exams, prescription eye glasses sunglasses, contact
         lenses, and top notch vitamins and drops especially to promote eye
         health!
-      </p>
-    ),
+      `,
     img: "/images/Rectangle 1-6.jpg",
     head: "Children/Senior Eye Exams",
   },
   {
-    img: "/images/AboutUs_Unsplash.png",
+    img: "/images/AboutUs_Unslapsh.png",
     head: "Children/Senior Eye exams",
-    text: (
-      <div>
-        <p>
+    text: `
+   
           Children under 19 and adults 65 and older are covered by MSP for their
           comprehensive eyes exams. Any other person between the ages of 18-65
           who is referenced from a family doctor or other specialist for a
           health condition will also be covered by MSP. *
-        </p>
-        <p>
+    
           Fees may be applicable. please call us or email us to inquire about
-          charges.
-        </p>
-      </div>
-    ),
+          charges.`,
   },
   {
     img: "/images/Rectangle 1-2.jpg",
@@ -41,20 +34,18 @@ export const cards = [
         Eye Exams, Glasses, & Contact lenses
       </>
     ),
-    text: (
-      <div>
+    text: `
         If you have private extended health insurance from one of the below
         companies, we offer direct billing for all glasses and contact lens
         orders. This means you can walk away with glasses or contacts with
         little to no out of- pocket expenses.
-        <p className="text-xs">
+      
           Most of our common health benefits providers are: Blue Cross, Chamber
           of Commerce, Crown, Desjardins, Great-West life, Canada Life,
-          Industrial Alliance, Johnson Inc., Manulife financial, Maximum
+          Industrial Alliance, Johnson Inc., Manulife financial, Maximun
           Benefit/Johnson Group, Standard Life, Sun Life Financial and more.
-        </p>
-      </div>
-    ),
+     
+   `,
   },
   {
     img: "/images/Rectangle 1.jpg",
@@ -88,10 +79,119 @@ export const cards = [
   },
 ];
 export const navArr = [
-  { name: "Bookings", path: "/bookings" },
   { name: "Home", path: "/" },
-  // { name: "COVID Safety", path: "/" },
-  { path: "/about-us", name: "About Us" },
+  { name: "Bookings", path: "/bookings" },
+  { name: "Promotions", path: "/promotions" },
   { path: "/service-products", name: "Services & Products" },
-  // { name: "Promotions", path: "/" },
+  { name: "Glasses & Contacts", path: "/glasses-contacts" },
+  { path: "/about-us", name: "About Us" },
+];
+
+export const eyeBrand = [
+  "Tome Ford",
+  "Marc Jacobs",
+  "Oakley",
+  "Fysh",
+  "Tory Burch",
+  "Silhouette",
+  "Wiley X",
+  "La Font",
+  "Eco",
+  "Kate Spade",
+  "Ray-Ban",
+  "Emporio Armani",
+  "Vogue",
+  "Coach",
+  "Hugo Boss",
+  "Tommy Hilfiger",
+  "Kliik",
+  "Rec Specs",
+  "Versace",
+  "Gucci",
+  "Maui Jim",
+  "Michael Kors",
+  "Tura",
+  "Stepper",
+];
+
+export const productCatalogue = [
+  {
+    img: "/images/man-lenses.jpg",
+    head: "Contact lenses",
+    text: "Experience clear vision with our wide selection of contact lenses. Perfect for any need, prescription or non-prescription available.",
+  },
+  {
+    img: "/images/man-eyeglass.jpg",
+    head: "Eyewears",
+    text: "Upgrade your style with our collection of trendy eyewears. From classic to modern, we have something for everyone.",
+  },
+  {
+    img: "/images/PVEC_eyemask.jpeg",
+    head: "Eye drops",
+    text: "Relieve dry, irritated eyes with our collection of high-quality eye drops. Perfect for daily use and for contact lens wearers.",
+  },
+];
+
+export const brandsArr = [
+  { link: "PVEC_Superflex.jpeg", addr: "" },
+
+  {
+    link: "PVEC_TF.png",
+    addr: "https://www.tomford.com/eyewear/men/optical/",
+  },
+  { link: "PVEC_RayBan.png", addr: "https://www.ray-ban.com/canada/en" },
+
+  {
+    link: "PVEC_EmporioArmani.jpeg",
+    addr: "https://www.armani.com/en-in/emporio-armani/man/eyewear",
+  },
+  { link: "PVEC_FYSH.jpeg", addr: "https://fyshuk.com" },
+  {
+    link: "PVEC_Gucci.png",
+    addr: "https://www.gucci.com/us/en/st/capsule/women-eyewear",
+  },
+  {
+    link: "PVEC_Guess.jpeg",
+    addr: "https://www.guess.com/us/en/women/accessories/sunglasses",
+  },
+  {
+    link: "PVEC_HugoBoss.png",
+    addr: "https://www.hugoboss.com/men-glasses/",
+  },
+  {
+    link: "PVEC_KateSpade.png",
+    addr: "https://www.katespade.com/shop/accessories/sunglasses-reading-glasses",
+  },
+  { link: "PVEC_KliikDenmark.jpeg", addr: "https://www.kliik.com/" },
+  { link: "PVEC_LaFont.png", addr: "https://www.lafont.com/the-collection/" },
+  {
+    link: "PVEC_MarcJacobs.jpeg",
+    addr: "https://www.marcjacobs.com/default/the-marc-jacobs/the-accessories/sunglasses/",
+  },
+  {
+    link: "PVEC_MichaelKors.jpeg",
+    addr: "https://www.michaelkors.ca/men/accessories/sunglasses/_/N-285v",
+  },
+  { link: "PVEC_Moleskine.png", addr: "https://moleskine-eyewear.com/en/" },
+  { link: "PVEC_Oakley.png", addr: "https://www.oakley.com/en-eu" },
+
+  { link: "PVEC_Staag.png", addr: "" },
+];
+
+export const banner = [
+  {
+    textColor: "black",
+    img: "banner-home-img",
+    text: "Our goal is to detect eye health issues before permanent damage occurs",
+  },
+  {
+    textColor: "white",
+    img: "banner-home-img-2",
+    text: "We offer the best suite of services and products",
+  },
+  {
+    textColor: "white",
+    img: "banner-home-img-3",
+    text: "Our goal is to detect eye health issues before permanent damage occurs",
+  },
 ];
