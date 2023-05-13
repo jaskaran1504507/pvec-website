6951a3f5a52a77107d991ace53cc9e9c36860a92
Shubham Sharma <shbh541@gmail.com>
2023-05-13 23:05:29 +0530

    Add date validation

--- nextjs-blog/pages/bookings/index.jsx
+++ nextjs-blog/pages/bookings/index.jsx
@@ -231,21 +231,15 @@ const Bookings = () => {
       <Banner
         banner="banner-bookings-img"
         text={
-          <h4 className="text-black md:text-white">
+          <span className="text-black md:text-white">
             Get a full check up
             <br />
             with one of our Optometrists, we're available evenings
             <br />
             and weekends too!
-          </h4>
+          </span>
         }
-      >
-        {/* <a href="#appointment">
-          <Button className="text-blue-800 bg-white">
-            BOOK MY APPOINTMENT
-          </Button>
-        </a> */}
-      </Banner>
+      />
       <div className="flex justify-center mt-10">
         <Form
           {...layout}
@@ -317,7 +311,7 @@ const Bookings = () => {
                 //   const start = moment("2020-01-01", "YYYY-MM-DD");
                 //   // return  current< start || current<moment();
                 //   console.log(moment(current).day(), "select");
-                return current < moment();
+                return current < moment() || current.day() === 0;
               }}
               format={"DD/MM/YYYY"}
               onChange={onChangeDate}
@@ -362,120 +356,6 @@ const Bookings = () => {
           </Form.Item>
         </Form>
       </div>
-      {/* <div
-        id="appointment"
-        className="u-clearfix u-sheet mx-auto u-valign-middle u-sheet-1 md:px-40"
-      >
-        <div className="flex items-center justify-center py-12 md:p-12">
-          <div className="mx-auto w-full max-w-[550px] bg-white">
-            <form
-              onSubmit={(ev) => {
-                ev.preventDefault();
-                alert("Appointment submitted");
-              }}
-              method="POST"
-            >
-              <div className="mb-5">
-                <label
-                  htmlFor="name"
-                  className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  required
-                  placeholder="Full Name"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                />
-              </div>
-              <div className="mb-5">
-                <label
-                  htmlFor="phone"
-                  className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                  Phone Number
-                </label>
-                <input
-                  required
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  placeholder="Enter your phone number"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                />
-              </div>
-              <div className="mb-5">
-                <label
-                  htmlFor="email"
-                  className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                  Email Address
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Enter your email"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                />
-              </div>
-              <div className="-mx-3 flex flex-wrap">
-                <div className="w-full px-3 sm:w-1/2">
-                  <div className="mb-5">
-                    <label
-                      htmlFor="date"
-                      className="mb-3 block text-base font-medium text-[#07074D]"
-                    >
-                      Date
-                    </label>
-                    <input
-                      type="date"
-                      name="date"
-                      id="date"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                    />
-                  </div>
-                </div>
-                <div className="w-full px-3 sm:w-1/2">
-                  <div className="mb-5">
-                    <label
-                      htmlFor="time"
-                      className="mb-3 block text-base font-medium text-[#07074D]"
-                    >
-                      Available slots
-                    </label>
-                    {/* <input
-                      type="time"
-                      name="time"
-                      id="time"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                    /> */}
-      {/* <select
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#0C4A6E] focus:shadow-md"
-                      name="slot"
-                      id=""
-                    >
-                      <option value="">01:00pm-02:00pm</option>
-                      <option value="">02:00pm-03:00pm</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div>
-                <button className="hover:shadow-form w-full rounded-md bg-sky-900 py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                  Book Appointment
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div> */}
-
-{/* <button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#success_tic">Open Modal</button> */}
       {showAppointmentSuccess && (
         <div id="success_tic" class="modal fade" role="dialog">
           <div className="modal-dialog">
@@ -490,7 +370,7 @@ const Bookings = () => {
                   <h4>Lorem ipsum dolor sit amet</h4>
                 </div>
 
-                <h1 style={{textAlign : 'center'}}>
+                <h1 style={{ textAlign: "center" }}>
                   <div className="checkmark-circle">
                     <div className="background"></div>
                     <div className="checkmark draw"></div>
