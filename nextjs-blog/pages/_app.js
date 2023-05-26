638a362d936641cdec96813590be5af8bf3c1fca
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-05-26 06:26:01 +0530

    tailwind css initial changes

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -13,6 +13,7 @@ import "./../pages/dummy/assets/css/bootstrap.min.css";
 import "./../pages/dummy/assets/css/default.css";
 import "./../pages/dummy/assets/css/style.css";
 import "./../components/FloatInput/index.css";
+import "../dist/output.css";
 import { useRouter } from "next/router";
 
 // This default export is required in a new `pages/_app.js` file.
@@ -27,7 +28,8 @@ export default function MyApp({ Component, pageProps }) {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
       ></link>
-      <script src="https://cdn.tailwindcss.com"></script>
+      {/* <script src="https://cdn.tailwindcss.com"></script> */}
+      <link href="/dist/output.css" rel="stylesheet"></link>
       <Head>
         <title>PV Eye Care</title>
         <link rel="icon" href="/pvec-small.ico" />
