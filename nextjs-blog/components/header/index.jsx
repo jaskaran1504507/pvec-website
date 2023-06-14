39b689a838168108dc11d885191a6b1618f509ee
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-06-14 13:02:47 +0530

    Update index.jsx

--- nextjs-blog/components/header/index.jsx
+++ nextjs-blog/components/header/index.jsx
@@ -192,7 +192,6 @@ export default function Header() {
                     <span className="toggler-icon"></span>
                     <span className="toggler-icon"></span>
                   </button>
-
                   <div
                     // style={{
                     //   background:
