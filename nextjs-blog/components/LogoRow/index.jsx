4710f0cec345b87b41d5cf8d5d22e399c757fb52
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-07-25 10:42:29 +0530

    Update index.jsx

--- nextjs-blog/components/LogoRow/index.jsx
+++ nextjs-blog/components/LogoRow/index.jsx
@@ -5,12 +5,12 @@ const LogoRow = ({ logos }) => {
         logos ?
        (<div className="flex flex-wrap justify-center bg-slate-100">
             {logos.map((logo, index) => (
-                    <div key={index} className="w-1/6 px-1 m-1">
-                        <Link href={logo.addr} target="_blank" className="m-1 w-11/12 h-20 center">
+                    <div key={index} className="w-1/6 m-1">
+                        <Link href={logo.addr} target="_blank" className="m-1 w-11/12 h-24 center">
                         <img
                             src={"/images/" + logo.link}
                             alt="Logo"
-                            className="w-4/5 h-full m-2"
+                            className="w-full h-full"
                         />
                         </Link>
                     </div>
