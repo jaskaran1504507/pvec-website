c6d941049f23bb075c2b73cbbb8e32edaab30bf5
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-10-04 23:07:45 +0530

    updated the website

--- nextjs-blog/components/LogoRow/index.jsx
+++ nextjs-blog/components/LogoRow/index.jsx
@@ -5,7 +5,7 @@ const LogoRow = ({ logos }) => {
     const router = useRouter();
     return (
         logos ?
-            (<div className="flex flex-wrap justify-center bg-slate-100">
+            (<div className="flex flex-wrap justify-center">
                 {logos.map((logo, index) => (
                     <div key={index} className="w-1/6 m-1">
                         <Link href={logo.addr} target="_blank" className={`m-1 w-11/12 center ${router?.query?.query == "eye-medic" ? 'h-48' : 'h-24'}`}>
