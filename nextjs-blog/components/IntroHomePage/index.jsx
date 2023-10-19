ea3159d8eeee173f15ec1de66fc305dabdf21be9
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-10-19 12:11:29 +0530

    Update the service and the intro panel

--- nextjs-blog/components/IntroHomePage/index.jsx
+++ nextjs-blog/components/IntroHomePage/index.jsx
@@ -30,71 +30,71 @@ export default function IntroHomePage() {
   }, []);
   return (
     <>
-    <div className="container md:mt-20 md:block hidden">
-      <div className="row justify-content-center">
-        <div className="w-full flex h-[80vh] relative">
-          <div id="obj1" className="w-1/2 mx-24 h-full flex">
-            <div className="bg-black mt-52 w-72 h-72">
-              <img
-                className="object-cover w-full h-full"
-                src="/images/water.jpg"
-                alt="Logo"
-              />
-            </div>
-            <div className="absolute top-0 left-0 ml-32 bg-blue-400 w-[55vh] h-[55vh]">
+      <div className="container md:mt-20 md:block hidden">
+        <div className="row justify-content-center">
+          <div className="w-[78vw] flex h-[80vh] relative">
+            <div id="obj1" className="w-1/2 h-full flex">
+              <div className="bg-black mt-64 w-72 h-72">
                 <img
-                className="object-cover w-full h-full shadow-xl shadow-black"
-                src="/images/bg-gl.jpg"
-                alt="Logo"
-              />
+                  className="object-cover w-full h-full"
+                  src="/images/water.jpg"
+                  alt="Logo"
+                />
+              </div>
+              <div className="absolute top-0 left-0 ml-6 bg-blue-400 w-[58vh] h-[58vh]">
+                <img
+                  className="object-cover w-full h-full shadow-xl shadow-black"
+                  src="/images/bg-gl.jpg"
+                  alt="Logo"
+                />
+              </div>
             </div>
-          </div>
-          <div id="obj2" className="w-1/2 h-full p-4 content-start font-mono">
-            <h2 className="title text-4xl uppercase ">Servicing Eyes on the Performance vision since 2004</h2>
-            <div className="content-start line m-auto h-1 bg-[#21AECA] mt-4 w-1/2"></div>
-            <div className="mt-8 leading-loose"><h3 className="title text-xl">Welcome to performance vision eye care, located at 1242 Burrard st,Vancouver, BC. <br />
-              We provide a full range of eye care services. From fashion-forward eyewear to family-oriented service, we are here to fulfill all of your eye health needs.</h3></div>
-            <div className="mt-20 text-xl">
-              <a className="page-scroll  rounded-lg hover:underline text-[#21AECA]" href="/bookings#appointment">
-                Request an appointment for your next eye exam.
-              </a>
+            <div id="obj2" className="w-1/2 h-full content-start font-mono">
+              <h2 className="title text-4xl uppercase ">Servicing Eyes on the Performance vision since 2004</h2>
+              <div className="content-start line m-auto h-1 bg-[#21AECA] mt-4 w-1/2"></div>
+              <div className="mt-8 leading-loose"><h3 className="title text-xl">Welcome to performance vision eye care, located at 1242 Burrard st,Vancouver, BC. <br />
+                We provide a full range of eye care services. From fashion-forward eyewear to family-oriented service, we are here to fulfill all of your eye health needs.</h3></div>
+              <div className="mt-20 text-xl">
+                <a className="page-scroll  rounded-lg hover:underline text-[#21AECA]" href="/bookings#appointment">
+                  Request an appointment for your next eye exam.
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container mt-12 mb-8 md:hidden block">
-      <div className="row justify-content-center">
-        <div className="w-full flex flex-col h-[80vh] ">
-          <div id="obj1" className="w-full h-full flex">
-            {/* <div className="bg-black mt-52 w-full h-full">
+      <div className="container mt-12 mb-8 md:hidden block">
+        <div className="row justify-content-center">
+          <div className="w-full flex flex-col h-[80vh] ">
+            <div id="obj1" className="w-full h-full flex">
+              {/* <div className="bg-black mt-52 w-full h-full">
               <img
                 className="object-cover w-full h-full"
                 src="/images/water.jpg"
                 alt="Logo"
               />
             </div> */}
-            <div className="bg-blue-400 w-full h-[55vh]">
+              <div className="bg-blue-400 w-full h-[55vh]">
                 <img
-                className="object-cover w-full h-full "
-                src="/images/bg-gl.jpg"
-                alt="Logo"
-              />
+                  className="object-cover w-full h-full "
+                  src="/images/bg-gl.jpg"
+                  alt="Logo"
+                />
+              </div>
             </div>
-          </div>
-          <div id="obj2" className="w-full h-full p-4 content-start font-mono">
-            <h2 className="title text-xl uppercase ">Servicing Eyes on the Performance vision since 2004</h2>
-            <div className="mt-8 leading-loose"><h3 className="title ">Welcome to performance vision eye care, located at 1242 Burrard st,Vancouver, BC. <br />
-              We provide a full range of eye care services. From fashion-forward eyewear to family-oriented service, we are here to fulfill all of your eye health needs.</h3></div>
-            <div className="mt-4 text-lg">
-              <a className="page-scroll  rounded-lg hover:underline text-[#21AECA]" href="/bookings#appointment">
-                Request an appointment for your next eye exam.
-              </a>
+            <div id="obj2" className="w-full h-full p-4 content-start font-mono">
+              <h2 className="title text-xl uppercase ">Servicing Eyes on the Performance vision since 2004</h2>
+              <div className="mt-8 leading-loose"><h3 className="title ">Welcome to performance vision eye care, located at 1242 Burrard st,Vancouver, BC. <br />
+                We provide a full range of eye care services. From fashion-forward eyewear to family-oriented service, we are here to fulfill all of your eye health needs.</h3></div>
+              <div className="mt-4 text-lg">
+                <a className="page-scroll  rounded-lg hover:underline text-[#21AECA]" href="/bookings#appointment">
+                  Request an appointment for your next eye exam.
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
