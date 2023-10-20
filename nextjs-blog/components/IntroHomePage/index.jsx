f2c68d984bd3d464be04cbd0ca25ce7e1e2c3fc9
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-10-20 09:18:09 +0530

    Update index.jsx

--- nextjs-blog/components/IntroHomePage/index.jsx
+++ nextjs-blog/components/IntroHomePage/index.jsx
@@ -50,14 +50,14 @@ export default function IntroHomePage() {
               </div>
             </div>
             <div id="obj2" className="w-1/2 h-full content-start font-mono">
-              <h2 className="title text-4xl uppercase ">Servicing since 2004</h2>
+              <h2 className="title text-4xl uppercase">Servicing since 2004</h2>
               <div className="w-full flex mt-3">
                 <div className="w-1/4 border-2 mt-8 border-[#23ADCA] m-auto">
                 </div>
                 <div className="w-3/4">
                 </div>
               </div>
-              <div className="mt-8 leading-loose"><h3 className="title text-xl">Welcome to performance vision eye care, located at 1242 Burrard st,Vancouver, BC. <br />
+              <div className="mt-8 leading-loose"><h3 className="title text-xl">Welcome to performance vision eye care, located at 1242 Burrard st, Vancouver, BC. <br />
                 We provide a full range of eye care services. From fashion-forward eyewear to family-oriented service, we are here to fulfill all of your eye health needs.</h3></div>
               <div className="mt-20 text-xl">
                 <a className="page-scroll  rounded-lg hover:underline text-[#21AECA]" href="/bookings#appointment">
