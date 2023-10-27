88cc417d649d5aed5188253ea45b219219c56dda
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-10-27 23:13:13 +0530

    Update index.jsx

--- nextjs-blog/components/LogoRow/index.jsx
+++ nextjs-blog/components/LogoRow/index.jsx
@@ -7,7 +7,7 @@ const LogoRow = ({ logos }) => {
         logos ?
             (<div className="flex flex-wrap justify-center">
             {logos.map((logo, index) => (
-                <div key={index} className="md:w-1/3 sm:w-1/2 py-4 px-2">
+                <div key={index} className="md:w-1/3 sm:w-1/2 py-4 px-2 border-2 border-gray-200">
                     <Link
                         href={router?.asPath.includes("#search_products") ? router?.asPath : router?.asPath + "#search_products"}
                         className={`block mx-auto ${router?.query?.query == "eye-medic" ? 'h-48' : 'h-24'}`}
