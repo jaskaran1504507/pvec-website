26fcb119a8ea7215d9b4a248ee24f41aeb2c140b
jaskaran1504507 <60620099+jaskaran1504507@users.noreply.github.com>
2023-11-16 03:44:59 +0530

    Update index.jsx

--- nextjs-blog/components/Slider/index.jsx
+++ nextjs-blog/components/Slider/index.jsx
@@ -41,7 +41,7 @@ const Slider = ({ slides }) => {
                                         className="w-full h-full object-cover center"
                                     />
                                 </div>
-                                <div className="backdrop-blur absolute md:top-96 top-48 left-0 right-0 bottom-0 flex items-center justify-center backdrop-blur backdrop-filter backdrop-saturate-150">
+                                <div className="backdrop-blur absolute md:top-80 left-0 right-0 bottom-0 flex items-center justify-center backdrop-blur backdrop-filter backdrop-saturate-150">
                                     <h1 className="font-serif font-bold md:text-6xl text-4xl font-bold text-sky-600">{slide.name}</h1>
                                 </div>
                             </div>
